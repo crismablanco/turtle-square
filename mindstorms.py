e5183f0a60097d574616dbc6451e693db30d95a0
crismablanco <crismablanco@gmail.com>
2015-12-04 15:38:04 -0300

    Turtle Square
    Uploading the First Python Drawing Project

---
+++ mindstorms.py
@@ -0,0 +1,17 @@
+import turtle
+
+def draw_square():
+    window = turtle.Screen()
+    window.bgcolor("black")
+    
+    brad = turtle.Turtle()
+    brad.shape("turtle")
+    brad.color("yellow")
+    for x in xrange(4):
+        brad.forward(100)
+        brad.right(90)
+        
+    
+    window.exitinclick()
+
+draw_square()
